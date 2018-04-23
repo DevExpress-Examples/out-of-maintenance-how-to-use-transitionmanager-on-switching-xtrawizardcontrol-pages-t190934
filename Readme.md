5d8d59a73fd607e2b521081fca6d133154d0c361
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to use TransitionManager on switching XtraWizardControl pages
+
+
+<p>This example demonstrates how to use our <a href="https://documentation.devexpress.com/#WindowsForms/clsDevExpressUtilsAnimationTransitionManagertopic">TransitionManager</a> to animate the <a href="https://documentation.devexpress.com/#WindowsForms/CustomDocument4735">XtraWizardControl</a> page switching.</p>
+
+<br/>
+
+
---
+++ VB/WindowsFormsApplication1/Form1.vb
@@ -0,0 +1,37 @@
+﻿Imports DevExpress.Utils.Animation
+Imports DevExpress.XtraWizard
+Imports System
+Imports System.Collections.Generic
+Imports System.ComponentModel
+Imports System.Data
+Imports System.Drawing
+Imports System.Linq
+Imports System.Text
+Imports System.Threading.Tasks
+Imports System.Windows.Forms
+
+Namespace WindowsFormsApplication1
+    Partial Public Class Form1
+        Inherits Form
+
+        Public Sub New()
+            InitializeComponent()
+        End Sub
+
+        Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs) Handles MyBase.Load
+            comboBoxEdit1.Properties.Items.Add(TransitionType.ClockTransition)
+            comboBoxEdit1.Properties.Items.Add(TransitionType.CombTransition)
+            comboBoxEdit1.Properties.Items.Add(TransitionType.CoverTransition)
+            comboBoxEdit1.Properties.Items.Add(TransitionType.DissolveTransition)
+            comboBoxEdit1.Properties.Items.Add(TransitionType.FadeTransition)
+            comboBoxEdit1.Properties.Items.Add(TransitionType.PushTransition)
+            comboBoxEdit1.Properties.Items.Add(TransitionType.ShapeTransition)
+            comboBoxEdit1.Properties.Items.Add(TransitionType.SlideFadeTransition)
+        End Sub
+
+        Private Sub comboBoxEdit1_SelectedIndexChanged(ByVal sender As Object, ByVal e As EventArgs) Handles comboBoxEdit1.SelectedIndexChanged
+            wizardControlAnimationHelper1.TransitionType = CType(comboBoxEdit1.SelectedItem, TransitionType)
+        End Sub
+
+    End Class
+End Namespace
